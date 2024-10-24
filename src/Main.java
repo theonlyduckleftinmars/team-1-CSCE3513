e946f9429d464bd4ac033ec9909bd9560987073c
Claire258 <112729820+Claire258@users.noreply.github.com>
2024-10-24 12:40:05 -0500

    Update Main.java
    Main updates:
    Removed UDPManager code, replaced with PhotonSeverSocket to utilize appropriate class
    Updated udpManager.receiveHits() to udp.addClientHandler()
    
    TO DO:
    Make sure this actually works

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ import network.*;
 public class Main {
     public static void main(String[] args) {
         SplashScreen splashScreen = new SplashScreen();
-        UDPManager udpManager = new UDPManager();
+        PhotonServerSocket udp = new PhotonServerSocket();
+        //UDPManager udpManager = new UDPManager();
         PlayerEntryScreen playerEntryScreen = new PlayerEntryScreen();
 
         splashScreen.display();
         playerEntryScreen.display();
-        udpManager.receiveHits();
+        udp.AddClientHandler();
+        //udpManager.receiveHits();
     }
 }
