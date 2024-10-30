fd16069aa600e1fe3f604c62e15473474abb0815
Claire258 <112729820+Claire258@users.noreply.github.com>
2024-10-30 18:49:24 -0500

    Update PlayActionScreen.java
    Added 6-minute game timer. Updated Action Log to be yellow so it stands out a bit more.

--- src/view/PlayActionScreen.java
+++ src/view/PlayActionScreen.java
@@ -10,6 +10,9 @@ import java.awt.event.ActionListener;
 
 public class PlayActionScreen {
 
+	//6 minutes = 360 seconds
+	private static final int GAME_TIMER = 360;
+	private int gameTimer = GAME_TIMER;
     private static final int TIME_REMAINING = 30;
     private JLabel countdownTimer;
     private Timer timer;
@@ -60,49 +63,97 @@ public class PlayActionScreen {
 
 
     private JPanel createActionLogPanel() {
-        JPanel actionLogPanel = new JPanel(new BorderLayout());
-        actionLogPanel.setBackground(DARK_BACKGROUND);
-        actionLogPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(LIGHT_TEXT), "Action Log"));
+		
+		JPanel actionLogPanel = new JPanel(new BorderLayout());
+		actionLogPanel.setBackground(DARK_BACKGROUND);
 
-        actionLogArea = new JTextArea();
-        actionLogArea.setEditable(false);
-        actionLogArea.setBackground(DARK_BACKGROUND);
-        actionLogArea.setForeground(LIGHT_TEXT);
-        actionLogArea.setFont(new Font("Arial", Font.PLAIN, 16));
+		// Create a label for the title
+		JLabel titleLabel = new JLabel("Action Log");
+		// Make it yellow to stand out a bit
+		titleLabel.setForeground(Color.YELLOW);
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
+		// Add some padding around the title
+		titleLabel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        JScrollPane scrollPane = new JScrollPane(actionLogArea);
-        scrollPane.setBorder(null);
-        actionLogPanel.add(scrollPane, BorderLayout.CENTER);
+		//Put it at the top
+		actionLogPanel.add(titleLabel, BorderLayout.NORTH);
 
-        return actionLogPanel;
+		actionLogArea = new JTextArea();
+		actionLogArea.setEditable(false);
+		actionLogArea.setBackground(DARK_BACKGROUND);
+		actionLogArea.setForeground(LIGHT_TEXT);
+		actionLogArea.setFont(new Font("Arial", Font.PLAIN, 16));
+
+		JScrollPane scrollPane = new JScrollPane(actionLogArea);
+		scrollPane.setBorder(BorderFactory.createLineBorder(LIGHT_TEXT));
+		actionLogPanel.add(scrollPane, BorderLayout.CENTER);
+
+		return actionLogPanel;
     }
+    
+    //Added for sprint 4 game timer
 
-    private void startCountdownTimer() {
-        timer = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                timeRemaining--;
-                countdownTimer.setText("Countdown to laser mayhem: " + timeRemaining);
-                countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
-
-                if (timeRemaining <= 0) {
-                    timer.stop();
-                    countdownTimer.setText("GAME STARTING");
-                    countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
-                    startGame();
-                }
-            }
-        });
-        timer.start();
-    }
-
-    private void startGame() {
-        System.out.println("Game started!");
-        logAction("Player1 shot Player2");
+    private void startCountdownTimer()
+    {
+		//Create the timer (uses milliseconds, so 1000 = 1 second)
+		timer = new Timer(1000, new ActionListener(){
+			@Override
+			//A second has passed!
+			public void actionPerformed(ActionEvent e){
+				//Decrease the timer
+				timeRemaining--;
+				//Display updated countdown
+				countdownTimer.setText("Countdown to laser mayhem: " + timeRemaining);
+				//Make sure that bad boy stays in the middle of the screen
+				countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
+				//The timer hit 0! Stop the timer, start the game
+				if(timeRemaining <= 0)
+				{
+					timer.stop();
+					countdownTimer.setText("GAME STARTING");
+					countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
+					startGame();
+				}
+			}
+		});
+		
+		timer.start();
+	}
+	private void startGameTimer()
+	{
+		timer = new Timer(1000, new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				gameTimer--;
+				countdownTimer.setText("Time remaining: " + gameTimer / 60 + " minutes " + gameTimer % 60 + " seconds");
+				countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
+				if(gameTimer <= 0)
+				{
+					timer.stop();
+					countdownTimer.setText("GAME ENDING");
+					countdownTimer.setHorizontalAlignment(SwingConstants.CENTER);
+					stopGame();
+				}
+			}
+		});
+		timer.start();
+	}
+	
+	//Timer has hit 0! Start the game
+	private void startGame()
+	{
+		System.out.print("Game started!");
+		logAction("Player1 shot Player2");
         logAction("Player3 shot Player4");
         // purely for testing rn!!!!
-    }
-
+		startGameTimer();
+	}
+	private void stopGame()
+	{
+		System.out.print("Game stopped");
+	}
+	
+	
     private void logAction(String action) {
         actionLogArea.append(action + "\n");
         actionLogArea.setCaretPosition(actionLogArea.getDocument().getLength()/2);
