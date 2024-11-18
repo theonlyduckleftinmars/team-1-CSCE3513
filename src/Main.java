2ad99ad455e93835301705d230d80750ced1b0d1
theonlyduckleftinmars <j.herdev1456@gmail.com>
2024-11-17 18:04:59 -0600

    test pss run method

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,12 @@ public class Main {
 
         playerManager.loadPlayers();
 
-        pss.run();
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
